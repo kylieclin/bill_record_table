1fb45535a032bc64861eaffcc05721d92f013392
Kylie Lin <kylieclin@gmail.com>
2019-03-27 16:46:50 -0700

    add student with dummy data

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,53 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import 'materialize-css/dist/js/materialize.min';
+import React, {Component} from 'react';
 import '../assets/css/app.scss'; //no matter where import style sheet it apply to the entire project
 import StudentsTable from './students_table';
+import AddStudent from './add_student';
+import studentsList from '../dummy_data/student_list';
 
-const App = () => (
-    <div>
-        <h1 className='center'>React SGT</h1>
-        <div className="row">
-            <StudentsTable />
-        </div>
+let id = 100;
+
+class App extends Component{
+    constructor(props){
+        super(props)
+
+        this.state ={
+            studentsList: []
+        }
+        this.addStudent = this.addStudent.bind(this);
+    }
+    addStudent(student){
+        student.id = id++;
         
-    </div>
-);
+        this.setState({
+            studentsList:[...this.state.studentsList, student]
+        })
+    }
+    componentDidMount(){
+        this.getStudentData();
+    }
+    getStudentData(){
+        //Call server
+
+        this.setState({
+            studentsList: studentsList
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1 className='center'>React SGT</h1>
+                <div className="row">
+
+                    <StudentsTable list={this.state.studentsList} col="col s12 m8"/>
+                    <AddStudent col="col s12 m4" callBack={this.addStudent}/>
+
+                </div>
+
+            </div>
+        )
+    }
+}   
 
 export default App;
