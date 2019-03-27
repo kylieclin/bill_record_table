c828d7b1243ac7f5cb823bdee238b521f1ca6763
Kylie Lin <kylieclin@gmail.com>
2019-03-27 12:55:10 -0700

    completed displaying student by using dummy data completed creating delete btn need to set up btn function

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 10,
+        name: 'Kevin Kent',
+        course: 'Basketball 101',
+        grade: 80
+    }, 
+    {
+        id: 1,
+        name: 'Vivian Le',
+        course: 'English 100',
+        grade: 56
+    },
+    {
+        id: 2, 
+        name: 'Steven Joe', 
+        course: 'Music 201', 
+        grade: 100
+    },
+    {
+        id: 3, 
+        name: 'Frank Smith', 
+        course: 'Math 103', 
+        grade: 77
+    }
+];
